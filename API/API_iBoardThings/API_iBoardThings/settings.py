6b704722862579c27fe6955d0717577422ca1811
Jouskaio <jouskaio.me@gmail.com>
2021-10-27 15:36:36 +0200

    Initizalisation back-end project

--- API/API_iBoardThings/API_iBoardThings/settings.py
+++ API/API_iBoardThings/API_iBoardThings/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+# Configuration django-environ
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+5ecdz^tieg@#g0a25w6uhj&f8h^)t9h*)p&8()w5i-*xcfi3z'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = []
 
@@ -76,7 +81,7 @@ WSGI_APPLICATION = 'API_iBoardThings.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'api.sqlite3',
     }
 }
 
